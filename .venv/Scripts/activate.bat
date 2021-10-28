c2bd64200841aceac48c98b867475cf5bf0326ae
Matheus <matheus@cda.lan>
2021-10-28 12:43:24 -0300

    Feat: Add readme and requirements

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\erick.felipe\documentacao\documentacao\.venv
+set VIRTUAL_ENV=C:\Users\matheus\Documents\cda\documentacao-mkdocs\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 

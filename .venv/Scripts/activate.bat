4311fa4b300088b232196e24afedb219ea851fd5
erickfelip <erickfelipe2010@gmail.com>
2021-10-28 21:15:38 -0300

    createpr

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\matheus\Documents\cda\documentacao-mkdocs\.venv
+set VIRTUAL_ENV=C:\Users\Erick\teste01\documentacao-mkdocs\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
